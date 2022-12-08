592c56b8967ac9b9b0e410fd643f348e080f665d
reyshazni013 <18220013@mahasiswa.itb.ac.id>
2022-12-08 15:55:18 +0700

    fix: change port

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ async def home():
     return {"message": "Welcome to Rey's API!"}
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8001,
+    uvicorn.run('main:app', host='0.0.0.0', port=8000,
                 log_level="info")
